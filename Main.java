eee5df06570b1e9b7a7e3d0796ddf5080b86ca40
bavatharani29 <135653435+bavatharani29@users.noreply.github.com>
2024-08-23 22:11:43 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first number");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter the second number");
+        int num2 = scanner.nextInt();
+        System.out.print("Enter the third number");
+        int num3 = scanner.nextInt();
+        int smallest = Math.min(num1,Math.min (num2, num3));
+        System.out.println("The smallest number is " + smallest);
+        scanner.close();
+
+        }
+    }
+
+
+
